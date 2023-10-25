155daa0159c1a32d2a9343e86221d73a819f84b6
xvnpw <17719543+xvnpw@users.noreply.github.com>
2023-10-25 10:06:16 +0200

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,16 @@
 
 echo "[start] ai.tm.py..."
 
-/usr/local/bin/python /app/ai-threat-modeling/ai-tm.py $1 --inputs "$2" --output "$3" -ai "$4" -atmi "$5" --model $6 --temperature $7 --verbose $8 --debug $9 -usos "${10}" -t "${11}" --provider "${12}"
+ARGS="$1 --inputs $2 --output $3 -ai $4 -atmi $5 --model $6 --temperature $7 -usos ${10} -t ${11} --provider ${12}"
+
+if $8 = 'true'; then
+    ARGS="$ARGS --verbose"
+fi
+
+if $9 = 'true'; then
+    ARGS="$ARGS --debug"
+fi
+
+echo $ARGS
+
+/usr/local/bin/python /app/ai-threat-modeling/ai-tm.py $ARGS
